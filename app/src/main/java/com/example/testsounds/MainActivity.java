5ee0033aac09d16a45b47e101588f664e55f565f
MadMakZa <immakza@gmail.com>
2020-12-30 22:46:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testsounds/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.testsounds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonPlay;
+    private MediaPlayer soundDices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        clickButton();
+    }
+
+    void init(){
+        buttonPlay = findViewById(R.id.btnPlaySound);
+        soundDices = MediaPlayer.create(this, R.raw.dice_sound1);
+    }
+    //метод нажатия кнопки
+    public void clickButton(){
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //запуск метода воспроизведения звука с переданным в него звуком
+                soundPlay(soundDices);
+            }
+        });
+    }
+    //метод воспроизведения звука для удобства
+    public void soundPlay(MediaPlayer sound){
+        sound.start();
+    }
+}
